4b897a2c1120c1b20be1b6dc4851c17a668d73b3
kurtiskubal <kurtisqball95@gmail.com>
2014-04-25 09:06:59 -0600

    Create GalToLit.java

---
+++ GalToLit.java
@@ -0,0 +1,19 @@
+/*
+* Converts Gallons to Liters
+* Author: Kurtis Kubal
+*/
+public class GalToLit {
+
+	public static void main(String args[]){
+		double gallons; //number of gallons
+		double liters; //number of liters
+
+		gallons = 10;
+		liters = gallons * 3.7854; // convert to liters
+
+		System.out.println(gallons + " gallons is " + liters + " liters.");
+		
+	}
+
+
+}
